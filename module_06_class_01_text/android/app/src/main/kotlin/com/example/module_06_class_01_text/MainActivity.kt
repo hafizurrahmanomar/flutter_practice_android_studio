4379aad9fc2edcb963d958d756647a2835d8b396
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-12-17 00:07:43 +0600

    Appbar And text widget

---
+++ module_06_class_01_text/android/app/src/main/kotlin/com/example/module_06_class_01_text/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_06_class_01_text
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ module_06_class_01_text/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //code here
+    return MaterialApp(
+      title: 'My First App',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement code here to build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        elevation: 5, //for shadow
+        leading: Icon(
+          Icons.home,
+          color: Colors.pinkAccent.shade700,
+          size: 45,
+        ),
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.pink),
+        ),
+      ),
+      //backgroundColor: Colors.amber,
+      backgroundColor: Color(0xFF4949D8),
+      body: Center(
+        child: Text(
+          'Alhamdulillah, My Flutter App run sucessfully.Tahnk you app web developer youtube channel'
+              .toUpperCase(),
+          //textAlign: TextAlign.justify,
+          textAlign: TextAlign.center,
+          maxLines: 2,
+          style: TextStyle(
+            //color: Colors.pink,
+            color: Color(0xFFffffff),
+            //backgroundColor: Colors.amber,
+            fontSize: 35,
+            fontWeight: FontWeight.w700,
+            wordSpacing: 5,
+            letterSpacing: 3,
+            //decoration: TextDecoration.lineThrough,
+            //decoration: TextDecoration.overline,
+            decoration: TextDecoration.underline,
+            //overflow: TextOverflow.visible,
+            //overflow: TextOverflow.clip,
+            //overflow: TextOverflow.fade,
+            overflow: TextOverflow.ellipsis,
+            decorationColor: Colors.pink,
+          ),
+        ),
+      ),
+    );
+  }
+}
