9e91c5728b80a9ce1bada5cbff7f14ba99999999
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-12-30 00:53:50 +0600

    fvm ok

---
+++ module_08_class_4_full_widget/android/app/src/main/kotlin/com/example/module_08_class_4_full_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.module_08_class_4_full_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ module_8_class_3_navigation_route_management/android/app/src/main/kotlin/com/example/module_8_class_3_navigation_route_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_8_class_3_navigation_route_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
