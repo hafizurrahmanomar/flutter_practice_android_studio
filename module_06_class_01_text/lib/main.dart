883dd4d1aaa989df734af8760ce965448ef72fca
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-12-30 13:59:24 +0600

    State Full Widget

--- module_06_class_01_text/lib/main.dart
+++ module_06_class_01_text/lib/main.dart
@@ -7,60 +7,97 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //code here
     return MaterialApp(
-      title: 'My First App',
-      home: HomeScreen(),
+      home: MyHome(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement code here to build
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
-        backgroundColor: Colors.amber,
-        elevation: 5, //for shadow
-        leading: Icon(
-          Icons.home,
-          color: Colors.pinkAccent.shade700,
-          size: 45,
-        ),
-        title: Text(
-          'Home',
-          style: TextStyle(color: Colors.pink),
+        title: Text('Full widget Practice'),
+        backgroundColor: Colors.pink,
+        elevation: 5,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {
+              // Handle back button press
+            },
+            icon: Icon(Icons.arrow_back_ios_new)),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Handle search button press
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              // Handle more options button press
+            },
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(40),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              'Subtitle or additional details',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
         ),
       ),
-      //backgroundColor: Colors.amber,
-      backgroundColor: Color(0xFF4949D8),
       body: Center(
-        child: Text(
-          'Alhamdulillah, My Flutter App run sucessfully.Tahnk you app web developer youtube channel'
-              .toUpperCase(),
-          //textAlign: TextAlign.justify,
-          textAlign: TextAlign.center,
-          maxLines: 2,
-          style: TextStyle(
-            //color: Colors.pink,
-            color: Color(0xFFffffff),
-            //backgroundColor: Colors.amber,
-            fontSize: 35,
-            fontWeight: FontWeight.w700,
-            wordSpacing: 5,
-            letterSpacing: 3,
-            //decoration:TextDecoration.overline,
-            //decoration: TextDecoration.lineThrough,
-            decoration: TextDecoration.underline,
-            //decorationThickness:BorderSide.strokeAlignCenter,
-            decorationColor: Color(0xFFff2def),
-            decorationStyle: TextDecorationStyle.dotted,
-            //overflow: TextOverflow.visible,
-            //overflow: TextOverflow.clip,
-            //overflow: TextOverflow.fade,
-            overflow: TextOverflow.ellipsis,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Full Widget Practice',
+              selectionColor: Colors.red,
+              style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.amber),
+            ),
+            SizedBox(height: 20),
+            FlutterLogo(
+              size: 100,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Handle button press
+              },
+              child: Text(
+                'Press Me',
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star_border, color: Colors.yellow),
+                Icon(Icons.star_border, color: Colors.yellow),
+                Icon(Icons.star, color: Colors.yellow),
+              ],
+            )
+          ],
         ),
       ),
     );
