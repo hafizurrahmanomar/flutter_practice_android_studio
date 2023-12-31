ff89e44a6f31ea8d9f5ca0581a9aa041e7cafa5c
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2024-01-01 03:01:56 +0600

    full state widget

--- module_08_class_4_full_widget/lib/main.dart
+++ module_08_class_4_full_widget/lib/main.dart
@@ -1,32 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeActivity(),
+      home: HomePage(),
     );
   }
 }
 
-class HomeActivity extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Full widget Practice'),
-        backgroundColor: Colors.pink,
-        elevation:5,centerTitle: true,
-
+        backgroundColor: Colors.teal,
+        elevation: 7,
+        leading: Icon(
+          Icons.home,
+          size: 35,
+          color: Colors.red,
+        ),
+        title: Text(
+          'Counter App with FullStateWidget Practice',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.overline,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              count.toString(),
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                count = count - 1;
+                setState(() {});
+              },
+              child: Text("Subtraction"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          count = count + 1;
+          setState(() {});
+        },
+        child: Icon(Icons.add),
       ),
     );
-    
   }
 }
